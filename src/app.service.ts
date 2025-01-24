a67e5aa41d48c80df8304abb98720efaeebfd0de
yuki <kyoiii0219@163.com>
2025-01-24 14:24:00 +0800

    import config type

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { LoggerService } from "./logger/logger.service";
+import { ConfigService } from "@nestjs/config";
+import { ConfigType } from "./config/config.type";
+import { AppConfig } from "./config/app.config";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly loggerService: LoggerService) {}
+  constructor(
+    private readonly loggerService: LoggerService,
+    private readonly configService: ConfigService<ConfigType>,
+  ) {}
 
   getHello(): string {
-    return `Hello World! ${this.loggerService.log("Hello World!")}`;
+    const prefix = this.configService.get<AppConfig>("app")?.messagePrefix;
+    return `${prefix} Hello World! ${this.loggerService.log("Hello World!")}`;
   }
 }
