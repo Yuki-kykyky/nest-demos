b81b0b696378fc9d80d6365b8190858a842141cd
yuki <kyoiii0219@163.com>
2025-02-06 16:35:52 +0800

    import match annotation to validate password

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -7,25 +7,25 @@ describe("CreateUserDto", () => {
     dto = new CreateUserDto();
     dto.email = "test@test.com";
     dto.name = "Piotr";
-    dto.password = "123456";
+    dto.password = "1234A@";
   });
   it("should validate complete valid data", async () => {
-    // 3xA
-    // Arrange
-    // Act
+    // when
     const errors = await validate(dto);
-    // Assert
+    // then
     expect(errors.length).toBe(0);
   });
 
   it("should fail on invalid", async () => {
-    // Arrange
+    // given
     dto.email = "test";
     dto.name = "";
     dto.password = "test";
-    // Act
+
+    // when
     const errors = await validate(dto);
-    // Assert
+
+    // then
     expect(errors.length).toBe(3);
     expect(errors[0].property).toBe("email");
     expect(errors[0].constraints).toHaveProperty("isEmail");
@@ -33,5 +33,30 @@ describe("CreateUserDto", () => {
     expect(errors[1].constraints).toHaveProperty("isNotEmpty");
     expect(errors[2].property).toBe("password");
     expect(errors[2].constraints).toHaveProperty("minLength");
+  });
+
+  const testPassword = async (password: string, message: string) => {
+    dto.password = password;
+    const errors = await validate(dto);
+    const passwordError = errors.find((error) => error.property === "password");
+    expect(passwordError).not.toBeUndefined();
+    const messages = Object.values(passwordError?.constraints ?? {});
+    expect(messages).toContain(message);
+  };
+
+  it("should fail without 1 uppercase letter", async () => {
+    await testPassword(
+      "abcdef",
+      "Password must contain at least 1 uppercase letter",
+    );
+  });
+  it("should fail without at least 1 number", async () => {
+    await testPassword("abdefaA", "Password must contain at least 1 number");
+  });
+  it("should fail without at least 1 special character", async () => {
+    await testPassword(
+      "abdefaA1",
+      "Password must contain at least 1 special character",
+    );
   });
 });
