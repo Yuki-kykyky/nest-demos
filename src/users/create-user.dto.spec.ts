c95d3896b3feebb2ec159f3636876a012a5a4fa1
yuki <kyoiii0219@163.com>
2025-02-06 16:18:45 +0800

    unhappy path test

--- src/users/create-user.dto.spec.ts
+++ src/users/create-user.dto.spec.ts
@@ -2,16 +2,36 @@ import { validate } from "class-validator";
 import { CreateUserDto } from "./create-user.dto";
 
 describe("CreateUserDto", () => {
-  it("should validate complete valid data", async () => {
-    // 3xA
-    // Arrange
-    const dto = new CreateUserDto();
+  let dto: CreateUserDto;
+  beforeEach(() => {
+    dto = new CreateUserDto();
     dto.email = "test@test.com";
     dto.name = "Piotr";
     dto.password = "123456";
+  });
+  it("should validate complete valid data", async () => {
+    // 3xA
+    // Arrange
     // Act
     const errors = await validate(dto);
     // Assert
     expect(errors.length).toBe(0);
+  });
+
+  it("should fail on invalid", async () => {
+    // Arrange
+    dto.email = "test";
+    dto.name = "";
+    dto.password = "test";
+    // Act
+    const errors = await validate(dto);
+    // Assert
+    expect(errors.length).toBe(3);
+    expect(errors[0].property).toBe("email");
+    expect(errors[0].constraints).toHaveProperty("isEmail");
+    expect(errors[1].property).toBe("name");
+    expect(errors[1].constraints).toHaveProperty("isNotEmpty");
+    expect(errors[2].property).toBe("password");
+    expect(errors[2].constraints).toHaveProperty("minLength");
   });
 });
